9334c1f4479f53447006570fcaa5214b380c6c13
manizkou <alertmanishrana@gmail.com>
2018-05-01 16:53:14 +0545

    new index

--- index.php
+++ index.php
@@ -1,49 +1,32 @@
  <html>
 	<head>
-		<title></title>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<style type="text/css">
+			.container{
+				background-color: #957BBE; 
+				padding: 10px;
+				min-height: 1000px;
+			}
+		</style>
 	</head>
 	<body>
 		<div class="container">
-			<div id="app">
-				<input type="text" name="" v-model="message">
-				<p> Adios {{message}} </p>
-				<div class="row">
-					<ul class="col-sm-6">
-						<li v-for="name in names" class="">
-						{{name}}
-						</li>
-					</ul>
-					<ul class="col-sm-6">
-						<li v-for="name in names" v-text="name" >
-						</li>
-					</ul>
+			<div class="row">
+				<div class="col-lg-4 ">
+					<input type="text" name="username">
+					<button type="button" class="btn btn-primary">Add Name</button>				
 				</div>
-				<input type="text" name="" id="input">
-				<button id="button">Add Name</button>
-				
+				<div class="col-lg-4 ">
+				</div>
 			</div>
 		</div>		
-		<script type="text/javascript" src="js/jquery-3.1.1.min.css"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/vue.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+
 
 		<script type="text/javascript">
-			new Vue({
-				el:'#app',
-				data: {
-					message: 'Hello World',
-					names:['Harry', 'James','Ron','Sirius']
-				},
-
-				mounted(){
-					// alert('Hello');
-				}
-			});
-
-			document.querySelector(#button).addEvetListener('click', () =>{
-				docume
-			});
 
 		</script>
 	</body>
